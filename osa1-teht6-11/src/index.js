893d07bcd81bea739dd919d03c63446ba0437d46
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-12-17 13:26:48 +0200

    add feedback system

--- osa1-teht6-11/src/index.js
+++ osa1-teht6-11/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Nappulat from './nappulat.js'
 
 class App extends React.Component {
     constructor (props){
         super(props);
 
-        this.muuta = this.muuta.bind(this);
+        this.handlefeedback = this.handlefeedback.bind(this);
 
         this.state = {
             hyva: 0,
@@ -14,35 +15,67 @@ class App extends React.Component {
         }
     }
 
-    muuta (nimi){
-        console.log(nimi)
+    handlefeedback (type){
+        this.setState({
+            [type]: this.state[type] + 1
+        })
     }
 
     render(){
+        let yhteen = (this.state.hyva + this.state.neutraali + this.state.huono);
+        let keskiarvo = (this.state.hyva * 1 + this.state.huono * -1) / yhteen;
+        let positiivista = this.state.hyva / yhteen * 100;
+        
+        keskiarvo = keskiarvo.toFixed(2);
+        positiivista = positiivista.toFixed(2);
+        let palaute = (<h4>Ei yhtään palautetta annettu</h4>);
+
+        let tableStyle = {
+            textAlign: 'left'
+        }
+
+        if (yhteen !== 0){
+            palaute = (
+            <table style={tableStyle}>
+                <tbody>
+                    <tr>
+                        <th>Hyvä:</th>
+                        <th>{this.state.hyva}</th>
+                    </tr>
+                    <tr>
+                        <th>Neutraali:</th>
+                        <th>{this.state.neutraali}</th>
+                    </tr>
+                    <tr>
+                        <th>Huono:</th>
+                        <th>{this.state.huono}</th>
+                    </tr>
+                    <tr>
+                        <th>Keskiarvo:</th>
+                        <th>{keskiarvo}</th>
+                    </tr>
+                    <tr>
+                        <th>Positiivista:</th>
+                        <th>{positiivista}%</th>
+                    </tr>
+                </tbody>
+            </table>)
+        }
+
         return (
             <div>
                 <h1>Anna palautetta</h1>
-                <Nappulat action={this.muuta} />
+                <Nappulat handlefeedback={this.handlefeedback} />
                 <h1>statistiikka</h1>
+                {palaute}
             </div>
         )
     }
 }
 
-const Nappulat = (props) => {
-    return (
-        <div>
-            <Nappula nimi={"Hyvä"} action={props.muuta}/>
-            <Nappula nimi={"Neutraali"} action={props.muuta}/>
-            <Nappula nimi={"Huono"} action={props.muuta}/>
-        </div>
-    )
-}
 
-const Nappula = (props) => {
-    return (
-        <button onClick={() => props.muuta("nimi")}>{props.nimi}</button>
-    )
-}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
