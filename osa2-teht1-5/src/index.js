4eda2d356765fde800421a1a8f55a441f9afb7f5
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-12-18 12:13:59 +0200

    copy course base code

--- osa2-teht1-5/src/index.js
+++ osa2-teht1-5/src/index.js
@@ -1,4 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = () => {
+    const kurssi = {
+      nimi: 'Half Stack -sovelluskehitys',
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Kurssi kurssi={kurssi} />
+      </div>
+    )
+  }
+
 ReactDOM.render(<App />, document.getElementById('root'));
