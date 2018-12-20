2c971a13890289e33df3e6ea35bd8ed26fc9c2ae
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-12-20 13:32:45 +0200

    Request data from RestCountries

--- osa2-teht12-13/src/index.js
+++ osa2-teht12-13/src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios'
+
+class App extends React.Component {
+    constructor (props){
+        super(props);
+
+        this.state = {
+            countries: [],
+            show: false,
+            name:''
+        }
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.getCountries = this.getCountries.bind(this);
+    }
+
+    componentDidMount(){
+        this.getCountries();
+    }
+
+    getCountries(){
+        axios.get('https://restcountries.eu/rest/v2/name/'+ this.state.name)
+        .then(response => {
+            console.log(response);
+            if(response.data.length < 11){
+                this.setState({
+                    countries: response.data,
+                    show: true
+                });
+            } else {
+                this.setState({
+                    countries:[],
+                    show: false
+                })
+            }
+        })
+    }
+
+    handleNameChange(e){
+        this.setState({
+            name: e.target.value
+        })
+        this.getCountries();
+    }
+
+    render(){
+        return(
+            <div>
+                find countries: <input onChange={this.handleNameChange} value={this.state.name}/>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
