5be4cd402aaa90f6676b7010072225fa8ea5897e
PaavoTL <paavo.linkala@hotmail.com>
2018-12-27 15:20:28 +0200

    added data removal and changing system

--- osa2-teht14-19/src/index.js
+++ osa2-teht14-19/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios'
+// import axios from 'axios'
+import server from './axios-module'
 import './index.css';
 import NumeroLista from './numerolista'
 import LisaaUusi from './lisaauusi'
@@ -19,15 +20,15 @@ class App extends React.Component {
       this.handleNumberChange = this.handleNumberChange.bind(this);
       this.handleFilterChange = this.handleFilterChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleRemove = this.handleRemove.bind(this);
     }
     
     componentDidMount(){
-        axios.get('http://localhost:3001/persons')
+        server.getAll()
         .then(response => {
             console.log(response);
-            this.setState({persons: [...response.data]
-            })
-        });
+            this.setState({persons: response});
+        })
     }
 
 
@@ -55,26 +56,62 @@ class App extends React.Component {
         e.nativeEvent.stopImmediatePropagation();
         
         if(this.state.persons.some(person => person.name === this.state.newName)){
-            alert(this.state.newName + ' already exists');
+            let answer = window.confirm(this.state.newName + " already exists. Replace old number?");
+            if(answer){
+                let number = this.state.newNumber;
+                let newArr = [...this.state.persons];
+                let id = 0;
+                
+                newArr.forEach(person => {
+                    id ++;
+                    if(person.name === this.state.newName){
+                        person.num = number;
+                     server.update(id, person);
+                    }
+                    return person;
+                })
+
+                this.setState({
+                    persons: [...newArr],
+                    newName: '',
+                    newNumber: ''
+                })
+            }
             return;
         }
         
         const newPerson = {name: this.state.newName, num: this.state.newNumber}
         const arr = this.state.persons;
+
+     server.create(newPerson)
         
         this.setState({
             persons: [...arr,newPerson],
-            newName: ''
+            newName: '',
+            newNumber: ''
         })
     }
   
+    handleRemove(id,name){
+        let answer = window.confirm(`Remove ${name} from the list?`)
+        if (answer) {
+            server.remove(id);
+            let newArr = [...this.state.persons];
+            newArr.splice(id-1,1)
+            
+            this.setState({
+                persons: [...newArr]
+            })
+        }
+    }
+    
     render() {
       return (
         <div>
             <h2>Puhelinluettelo</h2>
             rajaa näytettäviä: <input onChange={this.handleFilterChange} value={this.state.filter} />
             <LisaaUusi actions={[this.handleNameChange, this.handleNumberChange, this.handleSubmit]} values={[this.state.newName, this.state.newNumber]}/>
-            <NumeroLista list={this.state.persons} filter={this.state.filter} />
+            <NumeroLista list={this.state.persons} filter={this.state.filter} action={this.handleRemove} />
         </div>
       )
     }
